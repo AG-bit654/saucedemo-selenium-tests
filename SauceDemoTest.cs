d1354da064f85c9b1d96356824ab050e5479245b
Avdo Ganic <ganicavdo72@gmail.com>
2026-03-12 00:08:26 +0100

    Dodao sam Test 2

--- SauceDemoTest.cs
+++ SauceDemoTest.cs
@@ -15,11 +15,20 @@ namespace vjezba
 public void Test1_Login ()
 {
     Assert.IsNotNull(Driver.Instance, "Driver nije iniciliziran!");
-  SauceDemoPage sauceDemo = new SauceDemoPage (Driver.Instance);
+    SauceDemoPage sauceDemo = new SauceDemoPage (Driver.Instance);
     sauceDemo.Otvori();
     sauceDemo.Login("standard_user", "secret_sauce");
     Console.WriteLine(Driver.Instance.Url);
     Assert.IsTrue(Driver.Instance.Url.Contains("inventory"), "Login nije uspio!");
+}
+[TestMethod]
+public void Test2_pogresanLogin()
+{
+    Assert.IsNotNull(Driver.Instance, "Driver nije inicijaliziran!");
+    SauceDemoPage sauceDemo = new SauceDemoPage(Driver.Instance);
+    sauceDemo.Otvori();
+    sauceDemo.Login("pogresanUser", "pogresnaLozinka");
+    Assert.IsTrue(Driver.Instance.Url.Contains("saucedemo.com"), "Nešto nije u redu!");
 }
 }
 }
